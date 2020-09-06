ae0345c29681e115d205e8dd63b19119a855af31
Ala Rai AbdiAllah <alaraiabdiallah@gmail.com>
2020-09-06 20:28:23 +0700

    Slicing template default emam

---
+++ public/js/home.js
@@ -0,0 +1,50 @@
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+$(function() {
+    $(".food_search").keyup(debounce(function() {
+      var search_food = $(this).val();
+      var dataString = 'food_search=' + search_food;
+      if (search_food.length > 2) {
+        $.ajax({
+          type: "POST",
+          url: "/fetch_food.php",
+          data: dataString,
+          cache: false,
+          success: function(html) {
+            $("#result").html(html).show();
+          }
+        });
+      }
+      return false;
+    },250));
+
+    $("#result").on("click", function(e) {
+      var $clicked = $(e.target);
+      var $name = $clicked.find('.name').html();
+      var decoded = $("<div/>").html($name).text();
+      $('#search_food').val(decoded);
+    });
+
+    $(document).on("click", function(e) {
+      var $clicked = $(e.target);
+      if (!$clicked.hasClass("food_search")) {
+        $("#result").fadeOut();
+      }
+    });
+
+    $('#search_food').click(function() {
+      $("#result").fadeIn();
+    });
+});
