232a475d6f6adfa37a79a40a15d68571e66e7288
Ala Rai AbdiAllah <alaraiabdiallah@gmail.com>
2020-09-08 01:38:12 +0700

    https setting

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
+const http = require('http')
+const https = require('https');
+const fs = require('fs');
 const app = require('./bootstrap')
 const port = process.env.PORT || 3000
+const portSecure = process.env.PORT_SECURE || 443
 
-app.listen(port, () => {
+http.createServer(app).listen(port, () => {
   console.log(`Listen the app at :${port}`)
 })
+
+https.createServer({
+  key: fs.readFileSync('./cert/key.pem'),
+  cert: fs.readFileSync('./cert/cert.pem'),
+  passphrase: '6C76oymytoXk'
+}, app)
+.listen(portSecure,  () => {
+  console.log(`Listen the secure app at :${portSecure}`)
+});
